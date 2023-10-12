10baa095bf5769db9d40f855f4f35dff123c8d2b
nanana37@kuroServer <nakajima@keio.jp>
2023-10-12 03:14:43 +0000

    まとめて ret してるっぽい

--- test/example.c
+++ test/example.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 
 int my_open (int flag) {
+
     switch (flag) {
         case 0:
             printf("flag is 0\n");
@@ -18,6 +19,14 @@ int main(int argc, const char** argv) {
     int num;
     scanf("%d", &num);
     my_open(num);
+
+    switch (num) {
+        case 0:
+            printf("num is 0\n");
+            return -EACCES;
+        default:
+            break;
+    }
 
     return -EACCES;
 }
