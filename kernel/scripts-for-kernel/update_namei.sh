4ed8b62064d5679aed3db700f249219643e84a15
nanana37@kuroServer <nakajima@keio.jp>
2023-12-05 22:00:00 +0900

    update script for testing may_open

--- kernel/scripts-for-kernel/update_namei.sh
+++ kernel/scripts-for-kernel/update_namei.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-make -j 8 \
+rm fs/namei.o
+
+make -j 1 \
     CC=clang \
     KCFLAGS=" -fno-discard-value-names \
     -fpass-plugin=$HOME/eacces-modifier/build/permod/PermodPass.so" \
-    fs/namei.o \
-&& sudo make modules_install \
-&& sudo make install
+    fs/namei.o
