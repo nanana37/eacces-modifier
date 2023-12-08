650c10f0e85206f4738e9aed367a9731edcd0e62
nanana37@kuroServer <nakajima@keio.jp>
2023-12-08 14:52:00 +0900

    ammend rtlib

--- rtlib/rtlib.c
+++ rtlib/rtlib.c
@@ -4,8 +4,3 @@ void logcase(char *condition, int caseInt) {
     printf("[PERMOD] ");
     printf("%s is %d\n", condition, caseInt);
 }
-
-void logOfSwitch(char *condition, int *caseInt) {
-    printf("[PERMOD] ");
-    printf("%s is %d\n", condition, caseInt);
-}
