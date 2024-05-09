7b5305a089574b61dee701b038cda761921ef666
nanana37 <3na7nanana@gmail.com>
2024-05-09 15:32:54 +0900

    Ref: script to emit-ll for user's file

---
+++ scripts/emit_ll_example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# remember current dir
+CWD=$(pwd)
+
+# update pass
+cd ~/eacces-modifier/build
+echo "Updating pass"
+make
+
+cd $CWD
+# argument
+if [ $# -eq 0 ]; then
+	echo "No arguments supplied"
+	exit 1
+fi
+
+echo "Compiling $1"
+clang -g -fno-discard-value-names -fpass-plugin=$HOME/eacces-modifier/build/permod/PermodPass.so -emit-llvm -S $1
+echo "Done"
