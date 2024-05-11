f2461d7a18db65fd77c23782a2d6368933c5414b
nanana37 <3na7nanana@gmail.com>
2024-05-11 19:26:37 +0900

    Update while exapmle

--- test/example/while-example.c
+++ test/example/while-example.c
@@ -2,19 +2,27 @@
 #include <stdio.h>
 
 int errfunc(int x) {
+  printf("x = %d\n", x);
   int i = 0;
-  while (i < 5) {
-    printf("%d\n", i);
+  while (i != 5) {
     i++;
   }
-  if (x & 0) {
+  if (x & 1) {
     return -EACCES;
   }
   return 0;
 }
 
 int main() {
-  for (int i = 0; i < 5; i++) {
-    errfunc(i);
+  for (int i = 0; i < 3; i++) {
+    int ret = errfunc(i);
+    switch (ret) {
+    case -EACCES:
+      printf("EACCES\n");
+      break;
+    default:
+      printf("default\n");
+      break;
+    }
   }
 }
