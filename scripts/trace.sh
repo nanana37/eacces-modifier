7a1dd7c6220064249ec5077c55aecb0f71d77a38
nanana37 <3na7nanana@gmail.com>
2024-05-22 16:37:20 +0900

    Update trace script: execute command with arguments

--- scripts/trace.sh
+++ scripts/trace.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+# Usage: sh trace.sh <user> <command>
 
-sudo trace-cmd record -p function_graph --user $1 -F $2 && trace-cmd report trace.dat >trace.list
+user=$1
+shift
+command=$@
+
+echo "user=$user"
+echo "command=$command"
+
+sudo trace-cmd record -p function_graph --user $user -F $command && trace-cmd report trace.dat >trace.list
