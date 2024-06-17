0f7b16dab97e76d3d8ae9b8899a6c3643da03d2a
nanana37 <3na7nanana@gmail.com>
2024-06-17 14:05:23 +0900

    Update select example by adding condition

--- test/example/select-example.c
+++ test/example/select-example.c
@@ -1,17 +1,24 @@
 #include <errno.h>
 #include <stdio.h>
 
-int errfunc(int x) { return x % 2 == 0 ? 0 : -EACCES; }
+int errfunc(int x, int y) {
+  if (x & 2)
+    return y ? -EACCES : 0;
+  return 1;
+}
 
 int main() {
   int errno;
   for (int x = 0; x < 4; x++) {
-    printf("%d ", x);
-    errno = errfunc(x);
-    if (errno == 0) {
-      printf("No error\n");
-    } else {
-      printf("Error: %d\n", errno);
+    for (int y = 0; y < 2; y++) {
+      printf("x: %d y: %d\n", x, y);
+      errno = errfunc(x, y);
+
+      if (errno == 0) {
+        printf("No error\n");
+      } else {
+        printf("Error: %d\n", errno);
+      }
     }
   }
   return 0;
