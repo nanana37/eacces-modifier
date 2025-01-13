b4001375e19e41b59407b4b27d98bb1e57cc2355
na3@debifive <nakajima@keio.jp>
2025-01-14 01:00:22 +0900

    update scripts

--- scripts/update_kernel.sh
+++ scripts/update_kernel.sh
@@ -9,4 +9,4 @@ cd ~/eacces-modifier/build || exit
 make
 
 cd "$CWD" || exit
-make -j"$(nproc)" CC=clang LOCALVERSION=-$VERSION KCFLAGS="-g -fno-discard-value-names -fpass-plugin=$HOME/eacces-modifier/build/permod/PermodPass.so" && sudo make modules_install && sudo make install
+make -j"$(nproc)" CC=clang LOCALVERSION=-$VERSION KCFLAGS="-g -fno-discard-value-names -fpass-plugin=$HOME/eacces-modifier/build/permod/PermodPass.so" 2>$HOME/filter/tmp/permod-$(date +%Y%m%d) && sudo make modules_install && sudo make install
